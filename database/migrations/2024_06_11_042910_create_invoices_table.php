eaa7015d3538d9ae84f02d91d2796f4fd4133ff1
afifrohul <afifmemyself22@gmail.com>
2024-06-11 19:04:36 +0700

    update

--- database/migrations/2024_06_11_042910_create_invoices_table.php
+++ database/migrations/2024_06_11_042910_create_invoices_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            $table->foreignUuid('user_id');
+            $table->foreignUlid('user_id');
             $table->string('code');
             $table->integer('amount');
             $table->text('note');
